15068a540abadc75e363bc00cb700973078e83d3
dexteromen <himanshu@xenonstack.com>
2025-03-12 12:05:41 +0530

    Frontend Initiated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <h1>Hello</h1>
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
